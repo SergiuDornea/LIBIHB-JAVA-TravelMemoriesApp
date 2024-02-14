97774edff7db289e8d4f22d11a558d543985aaaa
SergiuDornea <88648596+SergiuDornea@users.noreply.github.com>
2024-02-14 22:34:06 +0200

    UI for Login and Auth screens 📱

--- app/src/main/java/com/sergiu/libihb_java/presentation/fragment/auth/AuthFragment.java
+++ app/src/main/java/com/sergiu/libihb_java/presentation/fragment/auth/AuthFragment.java
@@ -2,15 +2,22 @@ package com.sergiu.libihb_java.presentation.fragment.auth;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.navigation.Navigation;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import com.sergiu.libihb_java.R;
+import com.sergiu.libihb_java.databinding.FragmentAuthBinding;
+import com.sergiu.libihb_java.databinding.FragmentLogInBinding;
 
 public class AuthFragment extends Fragment {
+    FragmentAuthBinding binding;
+
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -20,7 +27,19 @@ public class AuthFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_auth, container, false);
+        binding = FragmentAuthBinding.inflate(inflater, container, false);
+        return binding.getRoot();
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        binding.logInHereLinkTextView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Navigation.findNavController(v).navigate(R.id.action_authFragment_to_logInFragment);
+            }
+        });
     }
 }
