ae4bd3b918ea2a0fbf5fbb772a25a3fba1d9ccea
SergiuDornea <88648596+SergiuDornea@users.noreply.github.com>
2024-04-21 17:22:13 +0300

    new screens added

---
+++ app/src/main/java/com/sergiu/libihb_java/presentation/fragment/favorites/FavoritesViewModel.java
@@ -0,0 +1,7 @@
+package com.sergiu.libihb_java.presentation.fragment.favorites;
+
+import androidx.lifecycle.ViewModel;
+
+public class FavoritesViewModel extends ViewModel {
+    // TODO: Implement the ViewModel
+}
