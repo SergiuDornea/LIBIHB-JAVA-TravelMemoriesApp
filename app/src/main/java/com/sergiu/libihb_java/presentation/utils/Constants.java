29b7b6a4b00043719d830333f55a32624d1598e2
SergiuDornea <dorneasergiu@gmail.com>
2024-09-15 17:35:50 +0300

    Update Constants.java

--- app/src/main/java/com/sergiu/libihb_java/presentation/utils/Constants.java
+++ app/src/main/java/com/sergiu/libihb_java/presentation/utils/Constants.java
@@ -11,11 +11,11 @@ public final class Constants {
     public static final String EXPLORE_POSITION_KEY = "explore_position_key";
     public static final String MEMORY_ID_KEY = "travel_id_key";
 
-    // https://api.openweathermap.org/data/2.5/weather?lat=44.34&lon=10.99&appid=091592242ee53d481c498231905b90ca
+
     public static final String OPEN_WEATHER_BASE_URL = "https://api.openweathermap.org/";
-    public static final String OPEN_WEATHER_API_KEY = "091592242ee53d481c498231905b90ca";
+    public static final String OPEN_WEATHER_API_KEY = "apikey";
     public static final String MOUNTAIN_API_BASE_URL = "https://mountain-api1.p.rapidapi.com/api/mountains/";
-    public static final String MOUNTAIN_API_KEY = "89f5282587mshbd6d6a0869a7cecp12199cjsnf2c06ef6c20a";
+    public static final String MOUNTAIN_API_KEY = "apikey";
     public static final String MOUNTAIN_RAPID_API = "X-RapidAPI-Key";
     public static final String MOUNTAIN_API_HOST = "mountain-api1.p.rapidapi.com";
     public static final String MOUNTAIN_API_HOST_NAME = "X-RapidAPI-Host";
